d8c5424e1aac2f1ee9accccea926181d4b4f2726
1451020071@dnu.edu.vn <1451020071@dnu.edu.vn>
2022-09-09 11:16:12 +0700

    setup app

---
+++ views/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿using ManagerCotrol.views.labels;
+using ManagerCotrol.views.panels;
+using ManagerCotrol.views.textbox;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ManagerCotrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+
+        private PanelView panelViewSignIn;
+        private int windowWidth, windowHeight;
+        public static string SIGN_IN = "sign_in";
+
+        private void InitializeComponent()
+        {
+            
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            this.Name = "Manager";
+            this.Text = "Manager";
+            this.WindowState = FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+            this.windowWidth = this.Width;
+            this.windowHeight = this.Height;
+
+            this.panelViewSignIn = new PanelView(0, 0, this.windowWidth, this.windowHeight);
+            this.panelViewSignIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelViewSignIn.BackColor = Color.Aqua;
+
+            setViewSignIn(panelViewSignIn, windowWidth / 2 - 400, windowHeight / 2 - 250, 800, 400);
+
+            this.SuspendLayout();
+
+            this.Controls.Add(this.panelViewSignIn);
+
+        }
+
+        private void setViewSignIn(Panel panel, int x, int y, int width, int height)
+        {
+
+            List<string> listLabelText = new List<string>();
+            listLabelText.Add("Username");
+            listLabelText.Add("Password");
+            listLabelText.Add("Permission");
+
+            PanelView panelView = new PanelView(x, y, width, height);
+            panelView.BackColor = Color.White;
+
+            LabelView labelView = new LabelView(panelView.PanelWidth / 2 - 100, 20, 200, 100, "Đăng nhập");
+            labelView.TextAlign = ContentAlignment.MiddleCenter;
+            labelView.Name = SIGN_IN;
+            labelView.Font = new Font("Arial", 24, FontStyle.Bold);
+            panelView.Controls.Add(labelView);
+
+            for (int i = 0; i < 3; i++)
+            {
+                LabelView viewInfo = new LabelView(50, labelView.LabelY + labelView.LabelHeight + i * 50, 150, 50, listLabelText[i]);
+                viewInfo.Name = listLabelText[i];
+                viewInfo.Font = new Font("Arial", 10, FontStyle.Regular);
+                panelView.Controls.Add(viewInfo);
+
+                TextBoxView textBoxInfo = new TextBoxView(50 + viewInfo.Width, labelView.LabelY + labelView.LabelHeight + i * 50, panelView.PanelWidth - 50 - (50 + viewInfo.Width), viewInfo.Height);
+                panelView.Controls.Add(textBoxInfo);
+
+            }
+
+
+
+            panel.Controls.Add(panelView);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ views/panels/PanelView.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagerCotrol.views.panels
+{
+    internal class PanelView : Panel
+    {
+        private int panelX;
+        private int panelY;
+        private int panelWidth;
+        private int panelHeight;
+        private List<Control> listChildView;
+
+        public int PanelX { get => panelX; set => panelX = value;        
+        }
+        public int PanelY { get => panelY; set => panelY = value; }
+        public int PanelWidth { get => panelWidth; set => panelWidth = value; }
+        public int PanelHeight { get => panelHeight; set => panelHeight = value; }
+        public List<Control> ListChildView { get => listChildView; set => listChildView = value; }
+
+        public PanelView()
+        {
+            
+            this.panelX = 0;
+            this.panelY = 0;
+            this.panelHeight = 0;
+            this.panelWidth = 0;
+            this.listChildView = new List<Control>();
+            this.SetBounds(panelX, panelY, panelWidth, panelHeight);
+        }
+        public PanelView(int x, int y, int width, int heigt)
+        {
+            this.panelX = x;
+            this.panelY = y;
+            this.panelWidth = width;
+            this.panelHeight = heigt;
+            this.listChildView = new List<Control>();
+            this.SetBounds(panelX, panelY, panelWidth, panelHeight);
+        }
+        public PanelView(int x, int y, int width, int heigt, List<Control> listChildView)
+        {
+            this.panelX = x;
+            this.panelY = y;
+            this.panelWidth = width;
+            this.panelHeight = heigt;
+            this.listChildView = listChildView;
+            this.SetBounds(panelX, panelY, panelWidth, panelHeight);
+        }
+
+
+        public void addChillView(Control value)
+        {
+            this.Controls.Add(value);
+        }
+
+        public void addAll(List<Control> list)
+        {
+            list.ForEach(control => this.Controls.Add(control));
+        }
+        public void clearChildView()
+        {
+            this.Controls.Clear();
+            this.listChildView.Clear();
+        }
+
+        
+    }
+}
